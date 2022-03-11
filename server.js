9c38bebe26e14412ee437845ec073d28d6a0051a
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-03-11 15:17:43 -0330

    Add id parameter

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ const PORT = process.env.PORT || 3001;
 // Modules
 const fs = require('fs');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-
-console.log(uuidv4());
+// const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 
-// API Routes
+// API Route to get saved notes from db.json file 
 app.get('/api/notes', (req, res) => {
    showNote(res);
 });
 
+// Route to save new notes to db.json file 
 app.post('/api/notes', (req, res) => {
     saveNewNote(req.body, res);
 });
 
+// Route to delete notes based on ID 
 app.delete('/api/notes/:id', (req, res) => {
     let id = req.params.id;
     const note = showNote(res);
@@ -62,6 +62,7 @@ const showNote = (res) => {
     const data = readFromDB();
     res.send(data);
 };
+// Reading db.json file and returning array of notes 
 const readFromDB = () => {
     let note = fs.readFileSync('./db/db.json', 'utf-8');
     return note = JSON.parse(note);
@@ -70,11 +71,14 @@ const readFromDB = () => {
 // Saving new note to database
 const saveNewNote = (newNote, res) => {
     const data = readFromDB();
+    let noteLength = (data.length).toString();
+    newNote.id = noteLength;
     data.push(newNote);
+
     writeToFile(data, res);
 };
+// Adding ID to new note and saving it to db.json database 
 const writeToFile = (data, res) => {
-    data = addUniqueId(data);
     data = JSON.stringify(data);
     fs.writeFile('./db/db.json', data, (err) => {
         if (err) {
@@ -83,13 +87,6 @@ const writeToFile = (data, res) => {
             res.send("New Note was saved successfully!");
         }
     });
-};
-const addUniqueId = (data) => {
-    var id = 1;
-    data.forEach(element => {
-        element["id"] = id++;
-    });
-    return data;
 };
 
 // Deleting notes 
